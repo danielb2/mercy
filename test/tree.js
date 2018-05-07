ac5ee4690fd5143efefd2246901743f7374a3677
Daniel Bretoi <daniel@bretoi.com>
2018-05-07 18:18:07 -0500

    Fixes

---
+++ test/tree.js
@@ -0,0 +1,195 @@
+'use strict';
+
+// Load modules
+
+const Code = require('code');
+const Hapi = require('hapi');
+const Hoek = require('hoek');
+const Joi = require('joi');
+const Lab = require('lab');
+const Insync = require('insync');
+
+const Mercy = require('../lib');
+
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const { describe, it } = lab;
+const expect = Code.expect;
+
+
+// Declare internals
+
+const internals = {
+    noop: (data, next) => { return next(); },
+    echo: (value, next) => { return next(null, value); },
+    console: (value, next) => {
+
+        console.log({value, next})
+        console.log({ console: value });
+        return next(null, value);
+    },
+    preRegister: (server, next) => {
+
+        server.route({
+            method: 'GET',
+            path: '/status',
+            handler: (request, reply) => {
+
+                // console.log({ server: request })
+
+                return reply({ status: 'ok' });
+            }
+        });
+
+        return next();
+    }
+};
+
+
+// {
+//     label: {
+//         data: {
+//             _meta: {}
+//             _input: []
+//             label: {
+//                 data: {
+//                     _meta: {}
+//                     _input: []
+//                     label: ...
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
+const Tree = require('../lib/tree');
+
+describe('Mercy', () => {
+
+    it('prints', (done) => {
+
+
+        const foo = function (data, next) { return next(); };
+        const series = Mercy.flow(Mercy.wait(32), Mercy.wait(32)).series();
+        const flow = Mercy.flow({ flow_task_0: foo, series })
+        const flow2 = Mercy.flow({ aFunction: foo, theflow: flow })
+
+        Mercy.execute(flow2, (err, meta, data, result) => {
+
+            flow2.tree(data);
+            done();
+        });
+    });
+
+    it('auto', (done) => {
+
+        const one = (data, next) => { return next(null, 'test1') };
+        const two = (data, next) => { return next(null, 'test2') };
+        const three = (one, two, next) => { return next(null, [one, two]) };
+
+        const auto = Mercy.flow({ one, two,  three: ['one', 'two', three] });
+
+        auto.tree();
+
+        done();
+    });
+});
+
+it.skip('complicated', (done) => {
+
+    const Crypto = require('crypto');
+
+    const internals = {
+        manifest: {
+            server: { load: { sampleInterval: 1000 } },
+            connections: [{
+                labels: ["api", "http"],
+                load: { maxHeapUsedBytes: 1073741824, maxRssBytes: 2147483648, maxEventLoopDelay: 5000 },
+                routes: { timeout: { server: 60000 } }
+            }],
+            registrations: []
+        },
+        preRegister: (server, next) => {
+
+            const results = [];
+            const rand = (request, reply) => {
+
+                const rand = Crypto.randomBytes(4).readUInt32LE(0);
+                results.push(rand);
+                return reply({ rand, results });
+            };
+
+            const status = (request, reply) => {
+
+                const status = 'ok';
+                results.push(status);
+                return reply({ status, results });
+            };
+
+            server.route([
+                { method: 'GET', path: '/rand', handler: rand },
+                { method: 'GET', path: '/status', handler: status }
+            ]);
+
+            return next();
+        }
+    };
+
+
+    // Basic Functions
+    // Note that `data` may be `last value` if injection occurs (series & auto)
+    const one = (data, next) => { return next(null, 'test1') };
+    const two = (data, next) => { return next(null, 'test2') };
+    const three = (one, two, next) => { return next(null, [one, two]) };
+
+    // A preapre() step to demonstrate additional complexity
+    const prepare = Mercy.prepare(internals.manifest, { preRegister: internals.preRegister });
+
+    // Series Flow
+    const series = Mercy.flow().series().tasks(one, two);
+
+    // Parallel Flow
+    const parallel = Mercy.flow().parallel().tasks(one, two);
+
+
+    // Main flow to execute
+    const flow = Mercy.flow().wait(200).tasks({     // Wait affects the root flow
+        input: Mercy.input(),                       // Ability to grab execute's input params and make easily accessible
+        prepare: prepare,                           // Consists of Series - [Mercy.compose(), Mercy.start()]
+        series: ['input', series],                  // Small series flow. `input` result is automatically injected
+        wait: ['series', Mercy.wait(200)],          // This is a wait flow()
+        parallel: ['wait', parallel]
+    }).final((data, next) => {
+
+        const result = {};
+        return next(null, result);
+    });
+
+    Mercy.execute(flow, (err, meta, data, result) => {
+
+        flow.tree(data);
+        done();
+    });
+});
+
+it('will not crash if funciton is null', (done) => {
+
+    const flow = Mercy.flow({
+        test: (data, next) => { return next(); },
+        test2: (data, next) => { return next(); },
+        test3: ['test', (data, next) => { return next(); }]
+    });
+
+    flow.tree();
+
+    done();
+});
+
+// root
+// ├── theFlow
+// │   └── task_0: foo
+// └── function: foo
